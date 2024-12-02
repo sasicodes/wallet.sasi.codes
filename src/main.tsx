a0d76b74d32f280be2312159774d74eb28aca4d7
sasicodes <hey@sasi.codes>
2024-12-02 18:47:22 +0530

    fix: update Toaster position in main component for improved user experience

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,11 @@ import { Toaster } from 'react-hot-toast'
 import './index.css'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div className="w-full max-w-2xl p-4 mx-auto md:px-0 md:py-10">
-      <Toaster position="top-right" />
+      <Toaster position="bottom-center" />
       <App />
     </div>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
